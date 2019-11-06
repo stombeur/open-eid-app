0e7f6e44007f9cec9084f65d5aa0b664ed576a0f
Open e-ID <open.eid@outlook.com>
2019-11-06 22:30:32 +0100

    Update open-eid.js

--- open-eid.js
+++ open-eid.js
@@ -153,7 +153,7 @@ try {
         pkcs11.C_CloseSession(session);                                 
         pkcs11.C_Finalize();  
         data = trim_data(data);        
-        var canJson = canonicalize(data);               
+        var canJson = JSON.canonify(data);               
         pkcs11 = new pkcs11js.PKCS11();
         pkcs11.load(options.lib_path);
         pkcs11.C_Initialize(); 
@@ -394,46 +394,45 @@ try {
     app.quit();
 }
 
-var canonicalize = function(object) {
-    var buffer = '';
-    serialize(object);
-    return buffer;
-    function serialize(object) {
-        if (object !== null && typeof object === 'object') {
-            if (Array.isArray(object)) {
-                buffer += '[';
-                let next = false;
-                // Array - Maintain element order
-                object.forEach((element) => {
-                    if (next) {
-                        buffer += ',';
-                    }
-                    next = true;
-                    // Recursive call
-                    serialize(element);
-                });
-                buffer += ']';
-            } else {
-                buffer += '{';
-                let next = false;
-                // Object - Sort properties before serializing
-                Object.keys(object).sort().forEach((property) => {
-                    if (next) {
-                        buffer += ',';
-                    }
-                    next = true;
-                    // Properties are just strings - Use ES6
-                    buffer += JSON.stringify(property);
-                    buffer += ':';
-                    // Recursive call
-                    serialize(object[property]);
-                });
-                buffer += '}';
-            }
-        } else {
-            // Primitive data type - Use ES6
-            buffer += JSON.stringify(object);
-        }
+;(function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require=="function"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error("Cannot find module '"+n+"'")}var u=t[n]={exports:{}};e[n][0](function(t){var r=e[n][1][t];return i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require=="function"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({1:[function(require,module,exports){
+JSON.canonify = function(v) {
+  var s, names, n, i;
+  if (Array.isArray(v)) {
+    s = '[';
+    for (i = 0; i < v.length; i++) {
+      if (i !== 0) {
+        s += ',';
+      }
+      s += typeof v[i] !== 'undefined' ? JSON.canonify(v[i]): 'null';
+      // an undefined array element is treated as null
     }
-};
+    s += ']';
+  }
+  else if (v !== null && typeof v === 'object') {
+    names = [];
+    for (n in v) {
+      if (v.hasOwnProperty(n) && typeof v[n] !== 'function') {
+        names.push(n);
+      }
+    }
+    names.sort();
+    s = '{';
+    names.forEach(function(val, i) {
+      if (i !== 0) {
+        s += ',';
+      }
+      s += JSON.stringify(val) + ':' + JSON.canonify(v[val]);
+    });
+    s += '}';
+  }
+  else {
+    s = JSON.stringify(v);
+  }
+  return s;
+}
+
+module.exports = JSON.canonify;
+
+},{}]},{},[1])
+;
 
