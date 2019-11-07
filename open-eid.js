ccb0dcde1ec888665fdbe39b5f3fb130e3df4b2d
Open e-ID <open.eid@outlook.com>
2019-11-07 02:34:39 +0100

    Update open-eid.js

--- open-eid.js
+++ open-eid.js
@@ -219,7 +219,7 @@ try {
         } else if(browser.toLowerCase().indexOf('\\firefox.exe') != -1) {
           cmd = '"' + browser + '" -width 300 -height 300 -new-window "' + url + '"';     
         } else if(browser.toLowerCase().indexOf('\\iexplore.exe') != -1) { // new window blocks localStorage -> new tab
-          fs.writeFileSync(path.join(os.homedir(), 'Open e-ID.html'), '<!DOCTYPE html>\r\n<html lang="en">\r\n<head>\r\n<meta charset="UTF-8">\r\n<!-- saved from url=(0016)' + new String(args).replace(proto, 'https') + ' -->\r\n<title>Open e-ID</title></head><body onload="window.open(decodeURIComponent(\'' + url.replace(/'/g, '\\\'') + '\'), \'' + ((wname == '') ? '_self' : wname) + '\');"></body></html>');
+          fs.writeFileSync(path.join(os.homedir(), 'Open e-ID.html'), '<!DOCTYPE html>\r\n<html lang="en">\r\n<head>\r\n<meta charset="UTF-8">\r\n<!-- saved from url=(0016)' + new String(args).replace(proto, 'https') + ' -->\r\n<title>Open e-ID</title></head><body onload="window.open(\'' + url.replace(/'/g, '\\\'') + '\', \'' + ((wname == '') ? '_self' : wname) + '\');"></body></html>');
           url = 'file:///' + path.join(os.homedir(), 'Open e-ID.html').replace(/ /g, '%20');
           cmd = 'cmd.exe /c start "' + browser + '" "' + url + '"';     
         } else {
