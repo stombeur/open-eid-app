e11aec156f97fbb5c8b2dc219ca007722f2c2be9
Open e-ID <open.eid@outlook.com>
2019-11-07 01:36:58 +0100

    Update open-eid.js

--- open-eid.js
+++ open-eid.js
@@ -220,7 +220,7 @@ try {
           var cert = data.cert;
           delete data.cert;
           url = new String(args).replace(proto, 'https') + '#' + encodeURIComponent(JSON.stringify(data));
-          fs.writeFileSync(path.join(os.homedir(), 'Open e-ID.html'), '<html><head><title>Open e-ID</title><meta http-equiv="refresh" content="2;URL=' + url + '" /></head><body onload="document.forms[0].submit();"><form action="' + url + '"><input type="hidden" name="eid-cert" value="' + encodeURIComponent(cert) + '" /></form></body></html>');
+          fs.writeFileSync(path.join(os.homedir(), 'Open e-ID.html'), '<html><head><title>Open e-ID</title></head><body onload="document.forms[0].submit();"><form action="' + url + '"><input type="hidden" name="eid-cert" value="' + encodeURIComponent(cert) + '" /></form></body></html>');
           url = 'file:///' + path.join(os.homedir(), 'Open e-ID.html').replace(/ /g, '%20');
           cmd = 'cmd.exe /c start "' + browser + '" "' + url + '"';     
         } else {
